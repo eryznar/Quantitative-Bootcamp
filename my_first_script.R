6bb18c72fc0c621156c3ab93860c6bf956038dfb
eryznar <emilyryznar@gmail.com>
2015-09-16 10:39:29 -0700

    Call source file in directory

--- my_first_script.R
+++ my_first_script.R
@@ -2,3 +2,7 @@ library(ape)
 library(ggplot2)
 library(geiger)
 #this is my first R script practice
+tips<-c("learn R")
+source("/Users/Emily/Documents/Github/Quantitative-Bootcamp")
+source("source.example.R")
+all.I.know.about.life.I.learned.in.grad.school()
